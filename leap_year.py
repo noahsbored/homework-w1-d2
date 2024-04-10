83fab1cabd712872bfaa1084a2e05401d29c0388
noahsbored <permannoach@gmail.com>
2024-04-10 16:57:07 -0400

    fixed mistake

--- leap_year.py
+++ leap_year.py
@@ -1,7 +1,7 @@
-leap = int(input('enter a year'))4
+leap = int(input('enter a year'))
 
 
-if leap % 4 == 0 and (leap %100 != 0 or year % 400 ==  0) :    
-    print (its a leap year)
+if leap % 4 == 0 and (leap %100 != 0 or leap % 400 ==  0) :    
+    print ('its a leap year')
 else:
     print ('not a leap year')
